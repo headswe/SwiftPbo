07e14a0c3c3aabbe3fec1fa5e64612bb9ad1ac4c
Sebastian Broberg <headswe92@gmail.com>
2015-09-01 11:57:11 +0200

    PboSwift

---
+++ PboTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwiftPbo;
+
+namespace PboTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cake = new Pbo();
+            cake.Load(@"C:\Users\Sebastian\Desktop\by_sea_by_land_v2.MCN_Aliabad.pbo");
+            cake.ExtractEntry(cake.Entries.Find(x => x.FileName == "f_assigngear_blu_f.sqf"), "config.cpp");
+
+        }
+    }
+}
