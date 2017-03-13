5fa53d402f10db1f5959eacb5153d926899fb3c9
Lecks <leckschester@hotmail.com>
2017-03-13 20:49:10 +1100

    Read as 'Default' encoding too

--- SwiftPbo/PboArchive.cs
+++ SwiftPbo/PboArchive.cs
@@ -309,7 +309,7 @@ namespace SwiftPbo
         private bool ReadEntry(FileStream stream)
         {
             var file = PboUtilities.ReadStringArray(stream);
-            var filename = Encoding.UTF8.GetString(file).Replace("\t","\\t");
+            var filename = Encoding.Default.GetString(file).Replace("\t", "\\t");
 
             var packing = PboUtilities.ReadLong(stream);
 
